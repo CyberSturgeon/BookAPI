8e3b9e018909160a44310fc1cbdd6752e70a4763
Nikolay <tentotennot@mail.ru>
2024-11-18 20:27:41 +0300

    fix

--- BooksAPI/Books.DAL/DTOs/User.cs
+++ BooksAPI/Books.DAL/DTOs/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Books.Core.DTOs;
+﻿namespace Books.DAL.DTOs;
 
 public class User
 {
