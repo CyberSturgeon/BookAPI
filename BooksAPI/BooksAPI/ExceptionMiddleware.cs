c5062e5fb90b4383f5e2d25f8d5f282accb554b7
Nikolay <tentotennot@mail.ru>
2024-12-07 20:24:40 +0300

    + exception middleware

---
+++ BooksAPI/BooksAPI/ExceptionMiddleware.cs
@@ -0,0 +1,97 @@
+﻿using Books.BLL.Exceptions;
+using System.Net;
+
+namespace BooksAPI;
+
+internal class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (EntityNotFoundException ex)
+        {
+            await HandleEntityNotFoundExceptionAsync(httpContext, ex);
+        }
+        catch (EntityConflictException ex)
+        {
+            await HandleEntityConflictExceptionAsync(httpContext, ex);
+        }
+        catch (WrongLoginException ex)
+        {
+            await HandleWrongLoginExceptionAsync(httpContext, ex);
+        }
+        catch (ArgumentException ex)
+        {
+            await HandleArgumentExceptionAsync(httpContext, ex);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private async Task HandleEntityNotFoundExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+
+    private async Task HandleEntityConflictExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+
+    private async Task HandleWrongLoginExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+
+    private async Task HandleArgumentExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Бэкендер обосрался!"
+        }.ToString());
+    }
+}
